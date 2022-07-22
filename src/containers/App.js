f5d6fc409273e3ab49fbe30507edcc651904864b
Andrew Liu <ahliu1@gmail.com>
2022-07-21 21:17:40 -0700

    redux middleware and thunk

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,65 +6,36 @@ import Scroll from '../components/Scroll';
 import ErrorBoundry from "../components/ErrorBoundry";
 import './App.css';
 
-import { setSearchField } from '../actions'
+import { requestRobots, setSearchField } from '../actions'
 
 // what piece of state I need to listen to and send it down as props
 const mapStateToProps = state => {
     return {
-        searchField: state.searchField // comes from our reducers
+        searchField: state.searchRobots.searchField, // comes from our reducers
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error,
     }
 }
 
 // what props I should listen to that are actions, that needs to get dispatched.
 const mapDispatchToProps = (dispatch) => {
     return { // return as an object that contains all of our action
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
     }
 }
 class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            // searchField: '',
-        }
-    }
-
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({robots: users})
-        );
+        this.props.onRequestRobots();
     }
-    // const [robots, setRobots] = useState([]);
-    // const [searchfield, setSearchField] = useState('');
-    // const [count, setCount] = useState(0);
-    
-    // useEffect(() => {
-    //     fetch('https://jsonplaceholder.typicode.com/users')
-    //         .then(response => response.json())
-    //         .then(users => setRobots(users)
-    //     );
-    //     console.log(count);
-    // }, [count]); // only run if count changes.
 
-
-    // const onSearchChange = (event) => {
-    //     setSearchField(event.target.value)
-    // }
-
-    // onSearchChange = (event) => {
-    //     this.setState({searchField: event.target.value})
-    // }
-
-    // const { robots, searchfield } = this.state;
     render() {
-        const { robots } = this.state;
-        const { searchField, onSearchChange } = this.props;
+        const { searchField, onSearchChange, robots, isPending } = this.props;
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField.toLowerCase())
         })
-        return !robots.length ?
+        return isPending ?
             <div className="tc"><h1 className="f1">Loading...</h1></div> : 
         (
             <div className="tc">
